ce5887a032554f4a4f184ff0c8aac4bda200d29b
Aniket <kaniket0814@gmail.com>
2024-07-25 21:30:30 +0530

    moved to folder

--- sumfunction.py
+++
@@ -1,6 +0,0 @@
-def sum (a,b):
-    return a+b 
-x=int(input("Enter the number first: "))
-y=int(input("Enter the number thirs: "))
-add = sum(x,y)
-print(add) 
--- sumofsquarerootofthree.py
+++
@@ -1,8 +0,0 @@
-import math
-n1 = int(input("Enter the first number: "))
-n2 = int(input("Enter the second number: "))
-n3 = int(input("Enter the third number: "))
-a = math.sqrt(n1)
-b = math.sqrt(n2)
-c = math.sqrt(n3)
-print(a+b+c)
